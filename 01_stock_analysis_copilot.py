ed5f9bb198656beccbfc1f1277cd9ec23e9375ef
Matt Dancho <mdancho@gmail.com>
2024-07-15 17:08:08 -0400

    product expert

--- 01_stock_analysis_copilot.py
+++ 01_stock_analysis_copilot.py
@@ -38,7 +38,6 @@ os.environ["TAVILY_API_KEY"] = yaml.safe_load(open("../credentials.yml"))['tavil
 
 tavily_tool = TavilySearchResults(max_results=5)
 
-# This executes code locally, which can be unsafe
 python_repl_tool = PythonREPLTool()
 
 # * Helper Utilities
@@ -136,12 +135,8 @@ code_node = functools.partial(agent_node, agent=code_agent, name="Coder")
 
 # * Construct Graph
 
-# The agent state is the input to each node in the graph
 class AgentState(TypedDict):
-    # The annotation tells the graph that new messages will always
-    # be added to the current states
     messages: Annotated[Sequence[BaseMessage], operator.add]
-    # The 'next' field indicates where to route to next
     next: str
 
 workflow = StateGraph(AgentState)
@@ -151,17 +146,13 @@ workflow.add_node("Coder", code_node)
 workflow.add_node("supervisor", supervisor_chain)
 
 for member in members:
-    # We want our workers to ALWAYS "report back" to the supervisor when done
     workflow.add_edge(member, "supervisor")
     
-# The supervisor populates the "next" field in the graph state
-# which routes to a node or finishes
-conditional_map = {k: k for k in members}
+conditional_map = {'Researcher': 'Researcher', 'Coder': 'Coder'}
 conditional_map["FINISH"] = END
 
 workflow.add_conditional_edges("supervisor", lambda x: x["next"], conditional_map)
 
-# Finally, add entrypoint
 workflow.set_entry_point("supervisor")
 
 graph = workflow.compile()
