5f692168f588e30e80838b11053310b5d66a20d0
Matt Dancho <mdancho@gmail.com>
2024-07-24 16:38:49 -0400

    stock analyzer complete

--- 00_stock_analysis_supervisor_agent/01_stock_analysis_copilot.py
+++ 00_stock_analysis_supervisor_agent/01_stock_analysis_copilot.py
@@ -1,7 +1,12 @@
-
+# BUSINESS SCIENCE UNIVERSITY
+# PYTHON FOR GENERATIVE AI COURSE
+# MULTI-AGENTS (AGENTIAL SUPERVISION)
+# ***
 
 # NOTE: requires yfinance to get the SPY data
 # NOTE: Requires Tavily API Key for Web Search (add to credentials.yml file)
+
+# * LIBRARIES
 
 from langchain.agents import AgentExecutor, create_openai_tools_agent
 from langchain_core.messages import BaseMessage, HumanMessage
@@ -29,6 +34,8 @@ from pprint import pprint
 from IPython.display import Image
 
 
+# * LLM SELECTION
+
 MODEL = "gpt-4o-mini"
 # MODEL = "gpt-3.5-turbo"
 # MODEL = "gpt-4o"
@@ -46,21 +53,19 @@ tavily_tool = TavilySearchResults(max_results=5)
 python_repl_tool = PythonREPLTool()
 
 # * Create Agent Supervisor
+#   - Supervisor has 1 role: Pick which team member to send to (or if finished)
 
 subagent_names = ["Researcher", "Coder"]
 
 system_prompt = (
-    "You are a supervisor tasked with managing a conversation between the"
-    " following workers:  {subagent_names}. Given the following user request,"
-    " respond with the worker to act next. Each worker will perform a"
-    " task and respond with their results and status. When finished,"
-    " respond with FINISH."
+    "You are a supervisor tasked with managing a conversation between the following workers:  {subagent_names}. Given the following user request, respond with the worker to act next. Each worker will perform a task and respond with their results and status. When finished, respond with FINISH."
 )
 
 # Our team supervisor is an LLM node. It just picks the next agent to process and decides when the work is completed
 
 # ['FINISH', 'Researcher', 'Coder']
 route_options = ["FINISH"] + subagent_names 
+route_options
 
 # Using openai function calling can make output parsing easier for us
 #  References: 
@@ -111,7 +116,9 @@ supervisor_chain
 
 # * SUBAGENTS
 
-def create_agent(llm: ChatOpenAI, tools: list, system_prompt: str):
+# * Helper function
+
+def create_agent_with_tools(llm: ChatOpenAI, tools: list, system_prompt: str):
     # Each worker node will be given a name and some tools.
     prompt = ChatPromptTemplate.from_messages(
         [
@@ -128,63 +135,61 @@ def create_agent(llm: ChatOpenAI, tools: list, system_prompt: str):
     return executor
 
 
-# * Research Agent
+# * Researcher Agent
 
-researcher_agent = create_agent(
+researcher_agent = create_agent_with_tools(
     llm, 
     [tavily_tool], 
     "You are a web researcher."
 )
 
-# * Code Agent
+researcher_agent
 
-coder_agent = create_agent(
+# * Coder Agent
+
+coder_agent = create_agent_with_tools(
     llm,
     [python_repl_tool],
     "You may generate safe python code to analyze data and generate charts using Plotly. Please share the specific details of the Python code in your reponse using ```python ``` markdown. Please make sure to use the plotly library.",
 )
 
-
+coder_agent
 
 # * LANGGRAPH
 
+#   - NEW Skill: Annotated Sequences
 class GraphState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], operator.add]
-    num_steps : int
+    num_steps: Annotated[Sequence[int], operator.add]
     next: str
     
     
 def supervisor_node(state):
     
-    return supervisor_chain
+    result = supervisor_chain.invoke(state)
+    
+    print(result)
+    
+    return {'next': result['next'], 'num_steps': 1}
 
 def research_node(state):
     
     result = researcher_agent.invoke(state)
     
-    return {"messages": [HumanMessage(content=result["output"], name="Researcher")]}
+    return {
+        "messages": [HumanMessage(content=result["output"], name="Researcher")],
+        'num_steps': 1
+    }
 
 def coder_node(state):
     
     result = coder_agent.invoke(state)
     
-    return {"messages": [HumanMessage(content=result["output"], name="Coder")]}
+    return {
+        "messages": [HumanMessage(content=result["output"], name="Coder")],
+        'num_steps': 1
+    }
 
-  
-# def state_printer(state):
-#     """print the state"""
-#     print("---STATE PRINTER---")
-#     print(f"Messages: {state['messages']}")
-#     print(f"Formatted Question (SQL): {state['formatted_user_question_sql_only']}")
-#     print(f"SQL Query: \n{state['sql_query']}\n")
-#     print(f"Data: \n{pd.DataFrame(state['data'])}\n")
-#     print(f"Chart or Table: {state['routing_preprocessor_decision']}")
-    
-#     if state['routing_preprocessor_decision'] == "chart":
-#         print(f"Chart Code: \n{pprint(state['chart_plotly_code'])}")
-#         print(f"Chart Error: {state['chart_plotly_error']}")
-    
-#     print(f"Num Steps: {state['num_steps']}")
 
 # * WORKFLOW DAG
 
@@ -213,11 +218,11 @@ Image(graph.get_graph().draw_mermaid_png())
 
       
 result_3 = graph.invoke(
-    input = {"messages": [HumanMessage(content="Find the historical prices of SPY for the last 5 years from Yahoo Finance (feel free to use the yfinance library, which is installed). Plot a daily line chart of the SPY value over time from the historical prices using python and the plotly library. Add a 50-day and 200-day simple moving average.")]},
+    input = {"messages": [HumanMessage(content="Find the historical prices of SPY for the last 5 years from Yahoo Finance (feel free to use the yfinance library, which is installed). Plot a daily line chart of the SPY value over time from the historical prices using python and the plotly library. Add a 50-day and 200-day simple moving average. Make sure the end date used is '2024-07-24'. Add a dateslider.")]},
     config = {"recursion_limit": 10},
 )
 
-pprint(dict(result_3), width=40, compact=True)
+result_3
 
 for message in result_3['messages']:
     if message.name:
@@ -231,6 +236,8 @@ result_4 = graph.invoke(
     input = {"messages": [HumanMessage(content="Find the historical prices of NVDA and VIX for the last 5 years from Yahoo Finance (feel free to use the yfinance library, which is installed). Plot a daily line chart of the value over time from the historical prices using python and the plotly library. Organize the plots by using 1 column by 2 row subplots so that the dates line up and VIX is the first plot and NVDA is below. Make sure the end date used is '2024-07-24'. Add a dateslider.")]},
     config = {"recursion_limit": 10},
 )  
+
+result_4
 
 for message in result_4['messages']:
     if message.name:
